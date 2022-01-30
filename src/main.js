3b8327e21e667107fd11906dff2fb036cb3c6383
Markuvines <shinigami_master@mail.ru>
2022-01-30 18:01:20 +0300

    added 'services' block

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+import $ from 'jquery'
 $(document).ready(function() {
-  
+  if ($(window).width() <= 564) {
+    console.log('fgdfgdfgdfg');
+    $('.blockHeader--left').removeClass('blockHeader--left')
+  }
+  $(window).resize(function() {
+    if ($(window).width() <= 564) {
+      console.log('fgdfgdfgdfg');
+      $('.blockHeader--left').removeClass('blockHeader--left')
+    } else {
+      $('.services__blockHeader').addClass('blockHeader--left')
+    }
+  })
+ 
 })
